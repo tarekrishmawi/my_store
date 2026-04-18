cc6a8c95f41a2bc78bd7d1df97fc11d2e2ac26d5
Tarek Rishmawi <rishmawitarek@gmail.com>
2026-04-18 15:21:09 +0300

    feat: add home and navbar components
    Add a home component to serve as the landing page and a navbar component for navigation. The navbar includes links to different sections of the application using ng Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 import { ProductList } from './components/product-list/product-list';
 import { ProductDetails } from './components/product-details/product-details';
 
 export const routes: Routes = [
-  { path: '', component: ProductList },
+  { path: '', component: Home, pathMatch: 'full' },
+  { path: 'products', component: ProductList },
   { path: 'products/:id', component: ProductDetails },
 ];
