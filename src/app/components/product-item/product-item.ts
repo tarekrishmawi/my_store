4c637d3a058a9456d9e4148cac203316c9b2fff4
Tarek Rishmawi <rishmawitarek@gmail.com>
2026-04-19 07:34:00 +0300

    style: Format price display using CurrencyPipe

--- src/app/components/product-item/product-item.ts
+++ src/app/components/product-item/product-item.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-item.html',
   styleUrl: './product-item.css',
 })
