ad0d81bb6b25bfc9f5c9f9d6e60cc2e30aaf5380
Tarek Rishmawi <rishmawitarek@gmail.com>
2026-04-19 08:04:03 +0300

    feat: Update navbar cart badge live
    Use BehaviorSubject and AsyncPipe to track and display the cart item count.

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  constructor(public cart: CartService) {}
   isMenuCollapsed = true;
 }
--- src/app/services/cart.ts
+++ src/app/services/cart.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   private items: Product[] = [];
+  private itemsCount = new BehaviorSubject<number>(0);
+  itemsCount$ = this.itemsCount.asObservable();
 
   add(product: Product) {
     this.items.push(product);
+    this.itemsCount.next(this.items.length);
   }
 
   getItems() {
@@ -17,10 +21,12 @@ export class CartService {
 
   remove(index: number) {
     this.items.splice(index, 1);
+    this.itemsCount.next(this.items.length);
   }
 
   clear() {
     this.items = [];
+    this.itemsCount.next(0);
   }
 
   total() {
